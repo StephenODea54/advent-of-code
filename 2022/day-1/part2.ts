cf679a36620e7f69b47658b930a14aed153dafbc
Stephen O'Dea <odeastephen1@gmail.com>
2022-12-02 20:45:32 +0000

    Moved `readInput` function into own import.

--- 2022/day-1/part2.ts
+++ 2022/day-1/part2.ts
@@ -1,6 +1,4 @@
-import { readFileSync } from "fs";
-
-const readInput = (filename: string) => readFileSync(filename, 'utf-8').split('\n');
+import readInput from "../functions/readInput";
 
 const calories = readInput('inputs.txt');
 
